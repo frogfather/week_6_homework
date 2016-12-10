960ff0a547444aab634ca17bd43055fad3a67027
John Campbell <j.a.campbell@icloud.com>
2016-12-10 17:02:01 +0000

    Added method to add dealer to game class

--- day_4/blackjack/Game.java
+++ day_4/blackjack/Game.java
@@ -1,13 +1,50 @@
 package blackjack;
 import java.util.*;
+import java.io.Console;
 
 public class Game{
-  Dealable deck;
-  Arraylist<Player> players;
-  Dealer dealer;
+  private Dealable deck;
+  private ArrayList<Player> players;
+  private Dealer dealer;
 
-  public Game(Dealable deck){
-  this.deck = new Deck;  
+  public Game(){
+  this.deck = new Deck();
+  this.players = new ArrayList<Player>();
+  setDealer();
+//  setPlayers();
   }
+
+  public void setDealer(){
+    Console console = System.console(); 
+
+    boolean named = false;
+    while (named == false){ 
+      String name = console.readLine("Please enter the name of the dealer... ");
+      if (name.length()>0){
+        this.dealer = new Dealer(name,this.deck);
+        named = true;
+        } 
+      }
+
+    boolean done = false;
+    while (done == false){
+      String input = console.readLine("Is the dealer also playing? y/n ");
+      if (input.equals("y")) {
+        addPlayer(this.dealer);
+        }
+      System.out.println("Players "+Integer.toString(getPlayerCount()));
+      done = ((input.equals("y"))||(input.equals("n"))); 
+      }
+
+  }
+
+  public void addPlayer(Player player){
+    this.players.add(player);
+  }
+
+  public int getPlayerCount(){
+    return this.players.size();
+    }
+
 }
 
--- day_4/blackjack/Runner.java
+++ day_4/blackjack/Runner.java
@@ -2,6 +2,7 @@ package blackjack;
 
 class Runner{
   public static void main(String[] args) {
-    System.out.println("This will be the program");
+    System.out.println("Welcome to blackjack");
+    Game game = new Game();
   }
 }
